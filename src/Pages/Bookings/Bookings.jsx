3071c1a022444bf246d808139219d3d4c1554d6e
Your Name <shahanricky86@gmail.com>
2023-05-16 17:17:07 +0600

    booking-added

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -11,7 +11,47 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, [url]);
+
+    const handleDelete = id => {
+        const proceed = confirm("Are you sure you want to delete")
+        if (proceed) {
+            fetch(`http://localhost:5000/booking/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert("deleted successfully")
+                        const remaining = bookings.filter(booking => booking._id !== id)
+                        setBookings(remaining)
+                    }
+                })
+        }
+    }
+
+    const handleUpdateBooking = id => {
+        fetch(`http://localhost:5000/booking/${id}`, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({ status: confirm })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged == true) {
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = "confirm";
+                    const newBooking = [updated, ...remaining];
+                    setBookings(newBooking)
+                }
+            })
+    }
+
     return (
         <div>
             <h1 className='text-4xl'>Your bookings {bookings.length}</h1>
@@ -25,23 +65,26 @@ const Bookings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
-                      {
-                        bookings.map(booking => <BookingRow
-                        key={booking._id}
-                        booking={booking}
-                        ></BookingRow>)
-                      }
-                        
+                        {
+                            bookings.map(booking => <BookingRow
+                                key={booking._id}
+                                booking={booking}
+                                handleDelete={handleDelete}
+                                handleUpdateBooking={handleUpdateBooking}
+                            ></BookingRow>)
+                        }
+
                     </tbody>
-                   
+
 
                 </table>
             </div>
